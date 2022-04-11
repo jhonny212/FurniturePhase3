9c7a6b1e781d9c097f5c3f974f0dd56739198cd4
jhonny212 <jhonnygarcia201830454@cunoc.edu.gt>
2022-04-11 12:20:44 -0600

    Test

--- inventoryService/src/main/java/com/furniture/inventoryService/Service/PieceService.java
+++ inventoryService/src/main/java/com/furniture/inventoryService/Service/PieceService.java
@@ -3,7 +3,10 @@ package com.furniture.inventoryService.Service;
 import com.furniture.inventoryService.Model.Piece;
 
 public interface PieceService {
-    public Piece createPiece(Piece piece);
+    public Piece createPiece(Piece piece,boolean bool);
     public Piece updatePiece(Piece piece);
     public Piece getPieceById(Integer id);
+    public boolean addInStock(Piece piece,int stock,double cost);
+    public boolean addInStock(int id,int stock,double cost);
+    public boolean removeInStock(int id, int amount);
 }
