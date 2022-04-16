33db577931940519666d3424cce85aed783cf0b6
Soberanis <jcsru13@gmail.com>
2022-04-16 12:29:36 -0600

    Update: trying to configure gateway to redirect to the other services

--- gatewayService/src/main/java/com/furniture/gatewayService/GatewayServiceApplication.java
+++ gatewayService/src/main/java/com/furniture/gatewayService/GatewayServiceApplication.java
@@ -2,6 +2,9 @@ package com.furniture.gatewayService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayServiceApplication {
@@ -10,4 +13,12 @@ public class GatewayServiceApplication {
 		SpringApplication.run(GatewayServiceApplication.class, args);
 	}
 
+	@Bean
+	public RouteLocator myRoutes(RouteLocatorBuilder builder){
+		return builder.routes()
+				.route(p -> p
+						.path("/admin/report/report-best-seller-x-period")
+						.uri("http://localhost:8084/admin/report/report-best-seller-x-period"))
+				.build();
+	}
 }
