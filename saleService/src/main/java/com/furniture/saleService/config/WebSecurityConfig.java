59f92a1ecf53193b20538f37c60d54ac818ce05a
Soberanis <jcsru13@gmail.com>
2022-04-25 22:38:52 -0600

    Fix: setting cors and authentication service for all services

--- saleService/src/main/java/com/furniture/saleService/config/WebSecurityConfig.java
+++ saleService/src/main/java/com/furniture/saleService/config/WebSecurityConfig.java
@@ -1,11 +1,9 @@
 package com.furniture.saleService.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.access.channel.ChannelProcessingFilter;
 
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -13,16 +11,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable().authorizeRequests()
-                //Dejamos abiertas todas las rutas, solo tendriamos que agregar la restricción de cors, para que solo reciba del gateway
-                .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
-                .antMatchers(HttpMethod.POST, "/**").permitAll()
-                .antMatchers(HttpMethod.GET, "/**").permitAll()
-                .antMatchers(HttpMethod.PUT,"/**").permitAll()
-                .antMatchers(HttpMethod.DELETE,"/**").permitAll()
-                //--------------------------------------------------------------------------------------------------------------------
-                .anyRequest().authenticated()
+                .anyRequest().permitAll()
                 .and()
+                .addFilterBefore(new CorsFilter(), ChannelProcessingFilter.class)
+                .addFilterAfter(new AuthFilter(), CorsFilter.class);
+                //--------------------------------------------------------------------------------------------------------------------
+                //.anyRequest().authenticated()
+                //.and()
                 //Agregamos el filtro por la configuración de cors
-                .addFilterBefore(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
+                //.addFilterBefore(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 }
---
+++ salesReportService/src/main/java/com/furniture/salesReportService/config/AuthFilter.java
@@ -0,0 +1,52 @@
+package com.furniture.salesReportService.config;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.server.ResponseStatusException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+@Component
+public class AuthFilter extends OncePerRequestFilter {
+
+    private RestTemplate restTemplate = new RestTemplate();
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        HttpHeaders headers = this.getHeaders(request);//Obtenemos los headers del que solicita
+        HttpEntity<Map<String, Object>> entity = new HttpEntity<>(new HashMap<>(), headers);
+        ResponseEntity<Boolean> authenticationResponse = restTemplate.postForEntity("http://localhost:8080/user/verifyJWT", entity, Boolean.class);
+
+        if(!authenticationResponse.getStatusCode().equals(HttpStatus.OK)){
+            System.out.println("Hubo un error aparentemente, estamos probando");
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private HttpHeaders getHeaders(HttpServletRequest request){
+        return Collections.list(request.getHeaderNames())
+                .stream()
+                .collect(Collectors.toMap(
+                        Function.identity(),
+                        h -> Collections.list(request.getHeaders(h)),
+                        (oldValue, newValue) -> newValue,
+                        HttpHeaders::new
+                ));
+    }
+}
